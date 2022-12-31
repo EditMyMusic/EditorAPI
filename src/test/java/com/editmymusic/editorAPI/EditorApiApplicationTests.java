caf75dd494f9c2f32834642193509b314a9f8368
Tony Mikhail <tonymikhail@Tonys-MacBook-Air.local>
2022-12-30 17:03:23 -0800

    add boiler plate files

---
+++ src/test/java/com/editmymusic/editorAPI/EditorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.editmymusic.editorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
